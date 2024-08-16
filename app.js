2149a85fd1b67b1dbc55859248628c11f830ff8d
HarryAhnHS <hongsuhahn@gmail.com>
2024-08-16 17:20:32 +0100

    dotenv

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 const path = require('path');
 var express = require('express');
 const {messagesRouter} = require('./routers/messagesRouter');
 
 var app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')))
@@ -12,6 +14,11 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use('/', messagesRouter);
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${port}/ !`);
